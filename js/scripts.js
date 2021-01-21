4300a2117bad3de049cc9d44e308c6c56959dc3a
burasovartem <morti.show@gmail.com>
2021-01-21 16:25:55 +0500

    reboot

--- js/scripts.js
+++ js/scripts.js
@@ -7,4 +7,29 @@ function change_nav(_id) {
         }
 	}
 	document.getElementById(_id).style.display = 'flex';
+	if (innerWidth <= 1200) {
+		menu.checked = false;
+		menu.onchange();
+	}
 }
+
+var menu = document.querySelector('.check_menu');
+menu.onchange = () => {
+	let nav = document.querySelector('.high_res'),
+		section = document.querySelector('#left_section'),
+		title = document.querySelector('.menu_title'),
+		node_title = document.querySelector('.low_res');
+  	if (menu.checked) {
+  		nav.style.display = 'flex';
+		section.style.height = '100vh';
+		document.body.style.overflow = 'hidden';
+		title.style.display = 'none';
+		node_title.style.justifyContent = 'flex-end';
+  	} else {
+  		nav.style.display = 'none';
+		section.style.height = 'auto';
+		document.body.style.overflow = 'auto';
+		title.style.display = 'block';
+		node_title.style.justifyContent = 'space-between';
+  	}
+};
